b6708f56b7b549c88de98f31544b6eb4c4c5e7f6
Robin Wyss <rowyss@gmail.com>
2013-05-08 10:21:25 +0200

    Initial Commit

---
+++ MemoryManager/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemoryManager
+//
+//  Created by Robin Wyss on 08.05.13.
+//  Copyright (c) 2013 Robin Wyss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
